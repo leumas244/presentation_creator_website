39d336aab16bea21fae5f0b2fc72a65d0960b8d4
Samuel <samuel.schlingheider@dm.de>
2021-11-18 14:17:11 +0100

    change project name

--- presentation_creator_website/asgi.py
+++ presentation_creator_website/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dashboard_template project.
+ASGI config for presentation_creator_website project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard_template.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'presentation_creator_website.settings')
 
 application = get_asgi_application()
--- presentation_creator_website/urls.py
+++ presentation_creator_website/urls.py
@@ -1,4 +1,4 @@
-"""dashboard_template URL Configuration
+"""presentation_creator_website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
