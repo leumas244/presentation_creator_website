163db13fd52d3705e9b2650ad30cb23a28b9730f
leumas244 <FreddiW.15>
2022-11-04 21:12:31 +0100

    add admin data from database

--- home/helper_package/helper_funktions.py
+++ home/helper_package/helper_funktions.py
@@ -4,22 +4,22 @@ from email.headerregistry import Address
 import datetime
 import pytz
 
-from .settings import email_user_name, email_password, email_user, email_error_receiver, name_error_reciever
-from home.models import Sended_Email
+from home.models import Sended_Email, AdminSetting
 
 
 def send_mail(receiver_name, receiver_mail, message, subject):
+    admin_settings = AdminSetting.objects.get(id=1)
     try:
         msg = EmailMessage() 
         msg["Subject"] = subject
-        msg["From"] = Address(email_user_name , addr_spec=email_user) 
+        msg["From"] = Address(admin_settings.email_user_name , addr_spec=admin_settings.email_user) 
         msg["To"] =  Address(receiver_name , addr_spec=receiver_mail)
 
         msg.set_content(message)
     
         server = smtplib.SMTP('smtp.strato.de', 587)
         server.starttls()
-        server.login(email_user, email_password)
+        server.login(admin_settings.email_user, admin_settings.email_password)
         server.sendmail(msg["From"], msg["To"], msg.as_string())
         server.quit()
 
@@ -34,9 +34,10 @@ def send_mail(receiver_name, receiver_mail, message, subject):
 
 
 def send_exeption_mail(traceback_details, username):
+    admin_settings = AdminSetting.objects.get(id=1)
     now = datetime.datetime.now()
     timezone = pytz.timezone("Europe/Berlin")
     right_time = pytz.utc.localize(now, is_dst=None).astimezone(timezone)
     mail_massage = "Hallo Samuel,\n\nEs gibt ein Problem bei der Präsentation-Webseite von der Stadtmission Grünstadt.\n\n" + "filename:  " + traceback_details['filename'] + "\nlinenumber:  " + traceback_details['lineno'] + "\nname:  " + traceback_details['name'] + "\ntype:  " + traceback_details['type'] + "\nmessage:  " + traceback_details['message'] + "\n\nAusgelöst von: " + str(username) + "\n\nProblem aufgetreten am: " + right_time.strftime("%d.%m.%Y, %H:%M:%S Uhr") + "\n\nViele Grüße\nAdmin"
     subject = "Problem bei der Präsentation-Webseite"
-    send_mail(name_error_reciever, email_error_receiver, mail_massage, subject)
+    send_mail(admin_settings.name_error_reciever, admin_settings.email_error_receiver, mail_massage, subject)
