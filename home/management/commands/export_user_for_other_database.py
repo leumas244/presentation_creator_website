c5db0d783e685a28ae2f7be923d79088960adc63
leumas244 <FreddiW.15>
2022-11-11 18:40:22 +0100

    export and import user fixes

--- home/management/commands/export_user_for_other_database.py
+++ home/management/commands/export_user_for_other_database.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import progressbar
 
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
@@ -10,13 +11,18 @@ class Command(BaseCommand):
     help = 'Export Users for other databases'
 
     def handle(self, *args, **options):
+        print('######## START EXPORT_USERS_FROM_DATABASE ########')
         now = datetime.datetime.now()
         time_for_output_file = now.strftime('%d.%m.%Y_%H-%M-%S')
         output_file = f'C:/Users/Samuel/Desktop/users_export_from_{time_for_output_file}.json'
         output_dictionary = {}
         list_of_users = []
         users = User.objects.all()
+        exported_user_counter = 0
+        counter = 0
 
+        bar = progressbar.ProgressBar(maxval=len(users), widgets=[progressbar.Bar('=', '    [', ']'), ' ', progressbar.Percentage()])
+        bar.start()
         for user in users:
             user_dic = {}
             add_user_info = AdditionalUserInfo.objects.get(user=user)
@@ -31,11 +37,25 @@ class Command(BaseCommand):
             user_dic["is_active"] = user.is_active
             user_dic["countdown_file_path"] = add_user_info.countdown_file_path
             user_dic["gender"] = add_user_info.gender
+            user_dic["one_time_token"] = add_user_info.one_time_token
+            if add_user_info.token_expiry_date:
+                user_dic["token_expiry_date"] = add_user_info.token_expiry_date.timestamp()
+            else:
+                user_dic["token_expiry_date"] = add_user_info.token_expiry_date
+            user_dic["has_loged_in"] = add_user_info.has_loged_in
 
             list_of_users.append(user_dic)
+            exported_user_counter += 1
+            counter += 1
+            bar.update(counter)
+        bar.finish()
         
         output_dictionary['users'] = list_of_users
+        try:
+            with open(output_file, 'w') as file:
+                json.dump(output_dictionary, file)
+        except Exception as e:
+            print(f'    Error by write dic in file ({output_file}) with error: "{str(e)}"')
 
-        with open(output_file, 'w') as file:
-            json.dump(output_dictionary, file)
+        print('######## FINISHING EXPORT_USERS_FROM_DATABASE ########')
 
--- home/management/commands/import_user_form_json_file.py
+++ home/management/commands/import_user_form_json_file.py
@@ -1,4 +1,6 @@
 import json
+import datetime
+import progressbar
 
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
@@ -9,13 +11,21 @@ class Command(BaseCommand):
     help = 'Migrate Users from other Databases'
 
     def handle(self, *args, **options):
-        json_file = f'C:/Users/Samuel/Desktop/test.json'
+        print('######## START IMPORT_USERS_FROM_JSON_FILE ########')
+        json_file = f'C:/Users/Samuel/Desktop/users_export_from_11.11.2022_18-30-36.json'
 
-        with open(json_file, 'r') as file:
-            data = json.load(file)
+        try:
+            with open(json_file, 'r') as file:
+                data = json.load(file)
+        except Exception as e:
+            print(f'    Error by load dic from file ({json_file}) with error: "{str(e)}"')
+            exit()
 
         users = data['users']
+        counter = 0
 
+        bar = progressbar.ProgressBar(maxval=len(users), widgets=[progressbar.Bar('=', '    [', ']'), ' ', progressbar.Percentage()])
+        bar.start()
         for user in users:
             username = user["username"]
             first_name = user["first_name"]
@@ -27,6 +37,12 @@ class Command(BaseCommand):
             is_active = user["is_active"]
             countdown_file_path = user["countdown_file_path"]
             gender = user["gender"]
+            one_time_token = user["one_time_token"]
+            token_expiry_date = user["token_expiry_date"]
+            if token_expiry_date is not None:
+                token_expiry_date = datetime.datetime.fromtimestamp(token_expiry_date)
+            has_loged_in = user["has_loged_in"]
+
 
             tracked_user = User.objects.filter(username=username)
             
@@ -40,7 +56,13 @@ class Command(BaseCommand):
                 created_user.is_superuser = is_superuser
                 created_user.save()
 
-                add_user_info = AdditionalUserInfo(user=created_user, countdown_file_path=countdown_file_path, gender=gender)
+                add_user_info = AdditionalUserInfo(user=created_user, countdown_file_path=countdown_file_path, gender=gender, 
+                                                   one_time_token=one_time_token, token_expiry_date=token_expiry_date, has_loged_in=has_loged_in)
                 add_user_info.save()
                 
-                print(f'Created User: {username} ({created_user.id})')
+                print(f'    Created User: {username} ({created_user.id})')
+            counter += 1
+            bar.update(counter)
+        bar.finish()
+
+        print('######## FINISHING IMPORT_USERS_FROM_JSON_FILE ########')
