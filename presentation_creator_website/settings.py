6b0a23c75eaf417d8d1a7209801946ed1732efd0
leumas244 <FreddiW.15>
2022-11-13 15:22:48 +0100

    disable DEBUG mode

--- presentation_creator_website/settings.py
+++ presentation_creator_website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l$7-^3)l_54a*+3+cr98-4m5xifj4g1_=!liowe*mr=gc=7g$r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
