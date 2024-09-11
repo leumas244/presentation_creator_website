e57b2954648b231b2a2768653b517f719a0bc7bc
Samuel - LeoCom PC <schlingheider@leocom.net>
2024-09-11 12:42:57 +0200

    Anpassung der Zeichens und des fehlers der top bar notification

--- static/base/js/topbar_notification_check.js
+++ static/base/js/topbar_notification_check.js
@@ -1,28 +1,33 @@
 function delete_notification(id) {
-    document.getElementById(id).remove()
-
-    emtpy_check();
+  document.getElementById(id).remove();
+  emtpy_check();
 }
 
 function emtpy_check() {
-    if (document.querySelector(".notification-wrapper ul").innerHTML.trim() != "") {
-        let element = document.getElementById("notification_icon");
-        element.classList.add("active");
-        document.getElementById("notification_button").style.cursor = "pointer";
-    } else {
-        let ul = document.getElementById('notification_list');
-        ul.remove();
-        let element = document.getElementById("notification_icon");
-        element.classList.remove("active");
-        document.getElementById("notification_button").disabled = true;
-        document.getElementById("notification_button").style.cursor = "default";
-    }
+  if (
+    document.querySelector(".notification-wrapper ul").innerHTML.trim() != ""
+  ) {
+    let element = document.getElementById("notification_icon");
+    element.classList.add("active");
+    document.getElementById("notification_button").style.cursor = "pointer";
+  } else {
+    let ul = document.getElementById("notification_list");
+    ul.remove();
+    let element = document.getElementById("notification_icon");
+    element.classList.remove("active");
+    document.getElementById("notification_button").disabled = true;
+    document.getElementById("notification_button").style.cursor = "default";
+  }
 }
 
-emtpy_check()
-if (document.querySelector(".notification-wrapper ul").innerHTML.trim() != "") {
-        let element = document.getElementById("notification_list");
-        let layer = document.querySelector('.layer');
-        element.classList.add("active");
-        layer.classList.add('active');
-};
+emtpy_check();
+if (document.querySelector(".notification-wrapper ul")) {
+  if (
+    document.querySelector(".notification-wrapper ul").innerHTML.trim() != ""
+  ) {
+    let element = document.getElementById("notification_list");
+    let layer = document.querySelector(".layer");
+    element.classList.add("active");
+    layer.classList.add("active");
+  }
+}
